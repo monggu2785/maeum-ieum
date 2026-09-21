f8bd7d47b10f6d4ab4a4ec8c17e959efe5587e72
mong <monggu8718@gmail.com>
2026-09-21 16:11:54 +0900

    Update execUrl to the correct Google Apps Script URL

--- js/schools.js
+++ js/schools.js
@@ -2,5 +2,5 @@ window.MI_SCHOOL = Object.freeze({
   code: 'HWASAN',
   name: '화산중학교',
   // Apps Script를 웹앱으로 배포한 뒤 /exec 주소를 아래에 붙여 넣으세요.
-  execUrl: 'PASTE_APPS_SCRIPT_WEB_APP_EXEC_URL_HERE'
+  execUrl: 'https://script.google.com/macros/s/AKfycbwdB_xtlyNSqdAtzuEXogpRS5UbHxCwMoQk8CNqxT1VHgDW0HuQO569B6fXJYtnBYgb9g/exec'
 });
